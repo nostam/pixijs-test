e4203f91b10bbbd7b19e5bba4380241d4bd6aee7
nostam <matsonho@gmail.com>
2021-12-30 07:25:50 +0100

    deploy to gh-pages

---
+++ index.ts
@@ -0,0 +1,5 @@
+export const assets = [
+    { name: "Clampy the clamp", url: "./clampy.png" },
+    // { name: "another image", url: "./monster.png" },
+    // { name: "whistle", url: "./whistle.mp3" },
+];
